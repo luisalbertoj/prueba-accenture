c68145382febef14d71a448226c656f99594df6d
Luis Jaimes <luisalbertoj.tober@gmail.com>
2021-04-08 22:17:34 -0500

    fix: iva

--- src/main/java/com/springboot/luis/services/BillServiceRest.java
+++ src/main/java/com/springboot/luis/services/BillServiceRest.java
@@ -6,9 +6,12 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.luis.models.Bill;
+import com.springboot.luis.models.User;
 
+@RestController
 public class BillServiceRest {
 	
 	private static List<Bill> listaFacturas = new ArrayList<Bill>() {{}};
@@ -16,13 +19,19 @@ public class BillServiceRest {
 	@RequestMapping(value="/bills", method=RequestMethod.POST)
     public Bill createBill(@RequestBody Bill newBill){
 		float domicilio = 10000.0f;
+		float iva = 0.19f;
 		if(newBill.getSubTotal() > 70000.0f) {
-			newBill.setTotal(newBill.getSubTotal() + newBill.getSubTotal()*0.19f + domicilio);
+			newBill.setTotal(newBill.getSubTotal() + newBill.getSubTotal()*iva + domicilio);
 		} else {
-			newBill.setTotal(newBill.getSubTotal() + newBill.getSubTotal()*0.19f);
+			newBill.setTotal(newBill.getSubTotal() + newBill.getSubTotal()*iva);
 		}
 		listaFacturas.add(newBill);
         return newBill;
     }
+	
+	 @RequestMapping(value="/bills", method=RequestMethod.GET)
+	 public List<Bill>  getBills(){
+        return listaFacturas;
+	 }
     
 }
--- src/main/java/com/springboot/luis/services/ProductServiceRest.java
+++ src/main/java/com/springboot/luis/services/ProductServiceRest.java
@@ -6,11 +6,13 @@ import java.util.List;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.springboot.luis.models.Product;
 
 
 
-
+@RestController
 public class ProductServiceRest {
 	
 	private static List<Product> listaProductos = new ArrayList<Product>() {
